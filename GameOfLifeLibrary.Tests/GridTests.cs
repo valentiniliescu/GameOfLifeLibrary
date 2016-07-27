4df7dfd4e59eb2efa3d6bcfe86249f6c7f9e7e18
valenti <Valentin Iliescu>
2016-07-27 15:29:11 -0700

    Added failed empty grid parse test; added Grid class

---
+++ GameOfLifeLibrary.Tests/GridTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLifeLibrary.Tests
+{
+    [TestClass]
+    public class GridTests
+    {
+        [TestMethod]
+        public void EmptyStringShouldParseToEmptyGrid()
+        {
+            var grid = Grid.Parse(string.Empty);
+            grid.NumberOfColumns.Should().Be(0);
+            grid.NumberOfRows.Should().Be(0);
+        }
+    }
+}
---
+++ GameOfLifeLibrary/Grid.cs
@@ -0,0 +1,13 @@
+﻿namespace GameOfLifeLibrary
+{
+    public class Grid
+    {
+        public static Grid Parse(string text)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int NumberOfColumns { get; set; }
+        public int NumberOfRows { get; set; }
+    }
+}
