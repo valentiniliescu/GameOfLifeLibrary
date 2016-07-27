1f2e7de8949941e13d80f9db5993aba1c06eed1c
valenti <Valentin Iliescu>
2016-07-27 16:19:45 -0700

    Added various unit tests for grid parsing

--- GameOfLifeLibrary.Tests/GridTests.cs
+++ GameOfLifeLibrary.Tests/GridTests.cs
@@ -34,5 +34,41 @@ namespace GameOfLifeLibrary.Tests
             grid[0, 1].Should().BeFalse();
             grid[1, 1].Should().BeTrue();
         }
+
+        [TestMethod]
+        public void Parse3X1Grid()
+        {
+            var grid = Grid.Parse("*.*");
+            grid.NumberOfColumns.Should().Be(3);
+            grid.NumberOfRows.Should().Be(1);
+            grid[0, 0].Should().BeTrue();
+            grid[1, 0].Should().BeFalse();
+            grid[2, 0].Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Parse1X3Grid()
+        {
+            var grid = Grid.Parse("*\n.\n*");
+            grid.NumberOfColumns.Should().Be(1);
+            grid.NumberOfRows.Should().Be(3);
+            grid[0, 0].Should().BeTrue();
+            grid[0, 1].Should().BeFalse();
+            grid[0, 2].Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Parse3X2Grid()
+        {
+            var grid = Grid.Parse("*.*\n***");
+            grid.NumberOfColumns.Should().Be(3);
+            grid.NumberOfRows.Should().Be(2);
+            grid[0, 0].Should().BeTrue();
+            grid[1, 0].Should().BeFalse();
+            grid[2, 0].Should().BeTrue();
+            grid[0, 1].Should().BeTrue();
+            grid[1, 1].Should().BeTrue();
+            grid[2, 1].Should().BeTrue();
+        }
     }
 }
