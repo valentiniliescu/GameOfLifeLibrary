341735fd50709934078c7b362913bf0507823edf
valenti <Valentin Iliescu>
2016-07-27 14:27:21 -0700

    Started the big solution in one step

--- GameOfLifeLibrary/GameOfLife.cs
+++ GameOfLifeLibrary/GameOfLife.cs
@@ -1,17 +1,32 @@
-﻿namespace GameOfLifeLibrary
+﻿using System.Linq;
+
+namespace GameOfLifeLibrary
 {
     public static class GameOfLife
     {
-        public static string GetNextGeneration(string grid)
+        public static string GetNextGeneration(string gridText)
         {
-            switch (grid)
+            var numberOfRows = gridText.Count(c => c == '\n') + 1; 
+            var numberOfColumns = gridText.IndexOf('\n') > -1 ? gridText.IndexOf('\n') : gridText.Length;
+
+            var grid = new bool[numberOfColumns, numberOfRows];
+
+            for (var index = 0; index < gridText.Length; index++)
             {
-                case "*":
-                case ".":
-                    return ".";
-                default:
-                    return string.Empty;
+                var c = gridText[index];
+                grid[index % numberOfColumns, index / (numberOfColumns + 1)] = c == '*';
             }
+
+            var newGrid = new bool[numberOfColumns, numberOfRows];
+            for (var col = 0; col < numberOfColumns; col++)
+            {
+                for (var row = 0; row < numberOfRows; row++)
+                {
+                    //newGrid[col, row] = formula of grid(col, row) and neighbors(grid(col, row))
+                }
+            }
+
+            return null; // return string representation of the grid
         }
     }
 }
