591d442ac3b210c7f5b6577c37519c36e8034a37
valenti <valenti@VALENTI-LAPTOP>
2016-09-26 18:11:20 -0700

    Made ParsingEmptyStringShouldReturn0X0Grid pass

--- GameOfLifeLibrary/Grid.cs
+++ GameOfLifeLibrary/Grid.cs
@@ -2,5 +2,15 @@ namespace GameOfLifeLibrary
 {
     public class Grid
     {
+        public override bool Equals(object obj)
+        {
+            var other = obj as Grid;
+            return other != null;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
     }
 }
--- GameOfLifeLibrary/GridSerialization.cs
+++ GameOfLifeLibrary/GridSerialization.cs
@@ -4,7 +4,7 @@
     {
         public static Grid Parse(string input)
         {
-            throw new System.NotImplementedException();
+            return new Grid();
         }
     }
 }
