74e66b44a9b6d161da4bf49c7ad67c2f61792ce4
valenti <valenti@VALENTI-LAPTOP>
2016-11-10 10:12:43 -0800

    Added failing CoordinatesNeighborsAndItself test

--- GameOfLifeLibrary/Coordinates.cs
+++ GameOfLifeLibrary/Coordinates.cs
@@ -19,6 +19,8 @@ namespace GameOfLifeLibrary
             new Coordinates(_row + 1, _column + 1)
         };
 
+        public Coordinates[] NeighborsAndItself => new Coordinates[0];
+
         public Coordinates(int row, int column)
         {
             _row = row;
