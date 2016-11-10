2888aba468bb4e8487ff8f6a07127b2d10a96da0
valenti <valenti@VALENTI-LAPTOP>
2016-11-10 08:48:49 -0800

    Make GridConstructorShouldInitializeCellsCoordinates pass

--- GameOfLifeLibrary/Coordinates.cs
+++ GameOfLifeLibrary/Coordinates.cs
@@ -4,13 +4,34 @@ namespace GameOfLifeLibrary
 {
     public class Coordinates
     {
-        public readonly int Row;
-        public readonly int Column;
+        private readonly int _row;
+        private readonly int _column;
 
         public Coordinates(int row, int column)
         {
-            Row = row;
-            Column = column;
+            _row = row;
+            _column = column;
+        }
+
+        protected bool Equals(Coordinates other)
+        {
+            return _row == other._row && _column == other._column;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Coordinates) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (_row*397) ^ _column;
+            }
         }
     }
 }
